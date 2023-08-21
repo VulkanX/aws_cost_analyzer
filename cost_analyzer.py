2177866d37e12073af7c2631c80a31e68d232b95
Will Schmidt <will@vulkan.ca>
2023-08-21 14:53:39 -0600

    Update
    Region Support for EBS Scan - Updated Cost_Analyzer.py

--- cost_analyzer.py
+++ cost_analyzer.py
@@ -1,7 +1,29 @@
-from lib.cost_s3 import S3_analyzer
+from lib.s3_analyzer_light import S3_analyzer
+from lib.ebs_analyzer import ebs_analyzer
 
 s3 = S3_analyzer()
 
+## Specifiy regions to analyze
+regions = [
+    'us-east-1',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2',
+    'ca-central-1',
+    'eu-west-1',
+    'eu-west-2',
+    'eu-central-1'
+]
+
+## Creates report showing Region, Size, Encryption, age and monthly cost
+## Exports: CSV
+print("EBS: Analyzing...")
+ebs = ebs_analyzer(regions)
+ebs.analyze()
+ebs.csv()
+
+## Creates report showing Region, Size, Number of Objects, Potential Costs, Lifecycle Policys, Replication Policies
+## Exports: CSV, HTML
 print("S3: Analyzing...")
 s3.analyze()
-s3.html()
+# s3.csv()
--- lib/ebs_analyzer.py
+++ lib/ebs_analyzer.py
@@ -5,38 +5,43 @@ import csv
 
 class ebs_analyzer:
 
-    def __init__(self, region_list = ['us-east-1']):
+    def __init__(self, region_list=['us-east-1']):
         self.snapshots = []
         self.region_list = region_list
 
     def analyze(self):
-        # Initialize AWS credentials and S3 client
-        ebs = boto3.client('ec2')
-        snapshots = ebs.describe_snapshots(OwnerIds=['self'])['Snapshots']
 
-        snapshot_data = []
+        # Loop through Regions
+        for region in self.region_list:        
 
-        # Flag Snapshots over 30 days old
-        for snapshot in snapshots:
-            snapshot_id = snapshot['SnapshotId']
-            snapshot_start_time = snapshot['StartTime']
-            snapshot_start_time = snapshot_start_time.replace(tzinfo=None)
-            snapshot_age = datetime.datetime.now() - snapshot_start_time
+            # Initialize AWS credentials and S3 client
+            ebs = boto3.client('ec2', region_name=region)
+            snapshots = ebs.describe_snapshots(OwnerIds=['self'])['Snapshots']
 
-            self.snapshots.append({
-                'SnapshotId': snapshot_id,
-                'SnapshotSize': snapshot['VolumeSize'],
-                'SnapshotCost': snapshot['VolumeSize'] * 0.05,
-                'SnapshotAge': snapshot_age.days,
-                'StorageTier': snapshot['StorageTier'],
-                'SnapshotEncryption': snapshot['Encrypted'],
-            })
+            snapshot_data = []
+
+            # Flag Snapshots over 30 days old
+            for snapshot in snapshots:
+                snapshot_id = snapshot['SnapshotId']
+                snapshot_start_time = snapshot['StartTime']
+                snapshot_start_time = snapshot_start_time.replace(tzinfo=None)
+                snapshot_age = datetime.datetime.now() - snapshot_start_time
+
+                self.snapshots.append({
+                    'Region': region,
+                    'SnapshotId': snapshot_id,
+                    'SnapshotSize': snapshot['VolumeSize'],
+                    'SnapshotCost': snapshot['VolumeSize'] * 0.05,
+                    'SnapshotAge': snapshot_age.days,
+                    'StorageTier': snapshot['StorageTier'],
+                    'SnapshotEncryption': snapshot['Encrypted'],
+                })
 
 
     def csv(self):
         # Write CSV
         with open('ebs.csv', 'w', newline='') as csvfile:
-            fieldnames = ['SnapshotId', 'SnapshotSize', 'SnapshotCost', 'SnapshotAge', 'StorageTier', 'SnapshotEncryption']
+            fieldnames = ['Region', 'SnapshotId', 'SnapshotSize', 'SnapshotCost', 'SnapshotAge', 'StorageTier', 'SnapshotEncryption']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             for snapshot in self.snapshots:
